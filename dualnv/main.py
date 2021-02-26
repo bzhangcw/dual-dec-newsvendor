fix gap calculation
add slides

dualnv/main.py
@@ -29,18 +29,18 @@ if __name__ == '__main__':
   evals = ['cv_func2']
 
   kwargs = {  # kwargs
-      "i": ni,
-      "t": nt,
-      "subproblem_alg_name": 'cppdp_batch',
-      "mp": False,
-      "proc": 0,
-      "mp_num": 4,
-      "gap": 0.005,
-      "scale": nt,
-      "max_iteration": 1000,
-      "eps_step": 1e-5,
-      "log_interval": 20,
-      # "evals": evals
+    "i": ni,
+    "t": nt,
+    "subproblem_alg_name": 'cppdp_batch',
+    "mp": False,
+    "proc": 0,
+    "mp_num": 4,
+    "gap": 0.005,
+    "scale": nt,
+    "max_iteration": 1000,
+    "eps_step": 1e-5,
+    "log_interval": 20,
+    # "evals": evals
   }
 
   scale = kwargs.get('scale')
@@ -54,17 +54,17 @@ if __name__ == '__main__':
   results = defaultdict(dict)
 
   methods = {
-      # "normal" convex sg
-      # "bran95_sg": {"r0": 1.2, "dual_option": "normal", "dir_option": "cvx", "hyper_option": "optimal", **kwargs},
-      "normal_sg": {
-          "r0": 1.2,
-          "dual_option": "normal",
-          "dir_option": "subgrad",
-          **kwargs
-      },
-      # "convex_sg": {"r0": 1.2, "dual_option": "normal", "dir_option": "cvx", **kwargs},
-      # "volume" convex sg
-      # "volume_sg": {"r0": 1.5, **kwargs}
+    # "normal" convex sg
+    # "bran95_sg": {"r0": 1.2, "dual_option": "normal", "dir_option": "cvx", "hyper_option": "optimal", **kwargs},
+    "normal_sg": {
+      "r0": 1.2,
+      "dual_option": "normal",
+      "dir_option": "subgrad",
+      **kwargs
+    },
+    # "convex_sg": {"r0": 1.2, "dual_option": "normal", "dir_option": "cvx", **kwargs},
+    # "volume" convex sg
+    # "volume_sg": {"r0": 1.5, **kwargs}
   }
 
   for i in range(instances):
@@ -83,40 +83,46 @@ if __name__ == '__main__':
     model_rel.optimize()
 
     instance_id = f"{num_i}_{num_t}_{timestamp}_{i}"
+    r['idx'] = results['idx'][i] = instance_id
     r['bench_lb'] = results['bench_lb'][i] = model.ObjBound
     r['bench_root_val'] = results['bench_root_val'][i] = model_rel.ObjVal
     r['bench_val'] = results['bench_val'][i] = model.ObjVal
     r['bench_time'] = results['bench_time'][i] = model.Runtime
-    r['id'] = instance_id
 
     for method, kw in methods.items():
       # run the method wrapper
       sol = subgrad_main.main(
-          problem, **kw)
+        problem, **kw)
       r[f"{method}_lb"] = results[f"{method}_lb"][i] = sol.lb[1:]
       r[f"{method}_val"] = results[f"{method}_val"][i] = sol.z_bar[1:]
       r[f"{method}_primal_k"] = results[f"{method}_primal_k"][
-          i] = sol.z_best[1:]
+        i] = sol.z_best[1:]
       r[f'{method}_time'] = results[f'{method}_time'][i] = sol.total_runtime
       for fc, v in sol.fc.items():
         plt.plot(v[1:], label=f"{method}_{fc}", linestyle='dashed')
       # save evaluations
       plt.legend(loc="lower left")
-      plt.savefig(f"fig/evals_{i}_{method}_{num_i}_{num_t}.png", dpi=500)
+      plt.savefig(f"fig/evals_{i}_{method}_{num_i}_{num_t}.png", dpi=1000)
       plt.clf()
       # save vals
+      plt.plot(
+        range(len(sol.lb) - 1),
+        r['bench_val'] * np.ones(len(sol.lb) - 1),
+        label=r'$f^\star$',
+        linestyle='dashed')
       for who in ['lb', 'val', 'primal_k']:
         length = len(r[f"{method}_{who}"])
         if who == 'primal_k':
           plt.plot(
-              range(length),
-              r[f"{method}_{who}"],
-              label=tex_alias.get(who),
-              linestyle='dashed')
+            range(length),
+            r[f"{method}_{who}"],
+            label=tex_alias.get(who),
+            linestyle='dashed')
         else:
           plt.plot(
-              range(length), r[f"{method}_{who}"], label=tex_alias.get(who))
-
+            range(length), r[f"{method}_{who}"], label=tex_alias.get(who))
+      plt.xlabel(r"iteration $k$")
+      plt.ylabel(r"primal and dual values")
       plt.legend(loc="lower left")
       plt.savefig(f"fig/conv_{i}_{method}_{num_i}_{num_t}.png", dpi=500)
       plt.clf()
@@ -131,9 +137,6 @@ if __name__ == '__main__':
         model.write(f"instances_pr/{instance_id}.lp")
         # DEBUG FOR PHI_LAMBDA
         py.test_phi_lambda(sol.lambda_k, problem, scale)
-
-
-
 
   # save results for analysis
   with open(f"instances/result_{instances}_{num_i}_{num_t}_{timestamp}.pk",

dualnv/pyrp/model_sg_alt.py
@@ -169,7 +169,7 @@ def dualnv_subgradient(problem,
   z_best = 1e6
   phi_best = -1e3
   # dual variable
-  lambda_k = lambda_b = np.zeros(scale)
+  lambda_k = lambda_b = np.ones(scale)
   # hyper parameters
   improved = 0
   improved_eps = param.improved_eps

dualnv/pyrp/util.py
@@ -17,7 +17,7 @@ def create_instance(nI: int, nT: int):
   T = range(nT)
   L = 2  # np.random.random() * 0.3
   U = np.random.randint(1, 10) + 12
-  D = np.random.randint(nI // 2, nI + 1, size=nT)
+  D = np.random.randint(nI // 2, 2 * nI + 1, size=nT)
   tau = np.random.randint(1, 2, nI)
   a = np.random.randint(2, 5, nI)
   b = np.random.randint(5, 10, nI)
@@ -25,12 +25,10 @@ def create_instance(nI: int, nT: int):
 
   # h = np.ones(nT) * 2
   # p = np.ones(nT) * 3
-  # c = np.random.randint(1, 3, nI)
-
   h = np.random.randint(1, 5, nT)
   p = np.random.randint(2, 6, nT)
   c = np.ones(nI)
-
+  # c = np.random.randint(2, 4, nI)
   return dict(
     D=D.astype(float).tolist(),
     a=a.astype(float).tolist(),
@@ -42,42 +40,6 @@ def create_instance(nI: int, nT: int):
     U=U,
     tau=tau.astype(int).tolist(),
     s0=s0.astype(float).tolist(),
-    h=h,
-    p=p)
-
-
-def create_instance_vec(nI: int, nT: int, nIns: int = 100):
-  """create randomly instances:
-    D is of shape (nIns, None)
-  Args:
-      nI (int): [description]
-      nT (int): [description]
-      nIns (int): number of samples
-  Returns:
-      [type]: [description]
-  """
-  I = [f"fl_{i}" for i in range(nI)]
-  T = range(nT)
-  L = np.random.random() * 0.3
-  U = np.random.random() * 0.5 + 1.2
-  # D = np.random.randint(nI // 2, nI, size=nT)
-  a = np.random.random(nI) * 0.5
-  b = np.random.random(nI) * 0.5 + 0.5
-  h = 1
-  p = 2
-  s0 = 1
-  D = np.random.randint(nI // 2, nI, size=(nIns, nT))
-  return dict(
-    Darr=D,
-    D=D.astype(float).tolist(),
-    a=a.tolist(),
-    b=b.tolist(),
-    I=I,
-    T=T,
-    L=L,
-    U=U,
-    tau=2,
-    s0=s0,
     h=h,
     p=p)
 